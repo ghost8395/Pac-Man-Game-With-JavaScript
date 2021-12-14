f9e8eab9ba5e2deabe316ede486544a3aadf0785
Manik Mudholkar <manik.mudholkar@cartus.com>
2021-12-14 23:07:18 +0530

    Gameboard class added

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-console.log('Working!');
+import { OBJECT_TYPE, LEVEL } from "./setup";
+import GameBoard from "./GameBoard";
+
+// DOM Elelments
+const gameGrid = document.querySelector("#game");
+const scoreTable = document.querySelector("#score");
+const startButton = document.querySelector("#start-button");
+
+// Game Constants
+const POWER_PILL_TIME = 10000; //ms
+const GLOBAL_SPEED = 80; //ms
+const gameBoard = GameBoard.createGameBoard(gameGrid, LEVEL);
+
+// Initial Setup
+let score = 0;
+let timer = null;
+let gameWin = false;
+let powerPillActive = false;
+let powerPillTimer = null;
+
+function gameOver(pacman, grid) {}
+
+function checkPosition(pacman, ghosts) {}
+
+function gameloop(pacman, ghosts) {}
+
+function startGame() {}
